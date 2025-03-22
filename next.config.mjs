e88164f2fd781ffecd5f4132b60f83df72adcf9d
Jan Gajdos <jangajdos@icloud.com>
2025-03-22 02:09:12 +0100

    Upravit next.config.mjs - nastavit output: export pro kompatibilitu se Supabase

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Vypnout statickou optimalizaci stránek a místo toho
+  // vygenerovat stránky na straně klienta
+  output: 'export',
+  images: {
+    unoptimized: true,
+  }
+};
 
 export default nextConfig;
